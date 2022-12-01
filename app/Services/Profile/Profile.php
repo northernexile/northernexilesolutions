b6353f1f34e945f4a06c86178198bacd438897ad
Northernexile <northernexile>
2022-12-01 01:28:15 +0000

    list users in profile

--- app/Services/Profile/Profile.php
+++ app/Services/Profile/Profile.php
@@ -3,12 +3,20 @@
 namespace App\Services\Profile;
 
 use App\Models\SkillType;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * @property null|Collection $skillTypes
+ * @property null|Collection $users
+ */
 class Profile implements \JsonSerializable
 {
     /** @var Collection|null  */
     protected ?Collection $skillTypes = null;
+
+    /** @var Collection|null  */
+    protected ?Collection $users = null;
 
     /**
      * @return Collection
@@ -23,11 +31,22 @@ class Profile implements \JsonSerializable
     }
 
     /**
+     * @return Collection
+     */
+    public function getUsers() :Collection
+    {
+        if(is_null($this->users)){
+            $this->users = User::orderBy('name')->get(['id','name','email']);
+        }
+    }
+
+    /**
      * @return Collection[]
      */
     public function toArray() :array
     {
         return [
+            'users' =>  $this->getUsers(),
             'skillTypes'=>$this->getSkillTypes()
         ];
     }
