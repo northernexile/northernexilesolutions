46df1649c2b3d82e394c97d9d97b92bb8b874455
Northernexile <northernexile>
2022-12-09 17:14:41 +0000

    add a state mechanism for availability

---
+++ resources/js/services/UserService.ts
@@ -0,0 +1,33 @@
+
+import http from "../http-common";
+import IUserData from "../types/user";
+
+const getAll = () => {
+    return http.get<Array<IUserData>>("/users");
+};
+
+const get = (id: any) => {
+    return http.get<IUserData>(`/users/${id}`);
+};
+
+const create = (data: IUserData) => {
+    return http.post<IUserData>("/users", data);
+};
+
+const update = (id: any, data: IUserData) => {
+    return http.put<any>(`/users/${id}`, data);
+};
+
+const remove = (id: any) => {
+    return http.delete<any>(`/users/${id}`);
+};
+
+const UserService = {
+    getAll,
+    get,
+    create,
+    update,
+    remove
+};
+
+export default UserService;
---
+++ resources/js/types/user.ts
@@ -0,0 +1,6 @@
+
+export default interface IUserData {
+    id?: any | null,
+    name: string,
+    email: string
+}
