2c068ead9015514db2adbffe4de5f1042133c1bb
Northernexile <northernexile>
2023-01-20 10:49:40 +0000

    add extra reducesrs to auth slice with builder

--- resources/js/redux/slices/authSlice.tsx
+++ resources/js/redux/slices/authSlice.tsx
@@ -14,6 +14,20 @@ const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {},
+    extraReducers: builder => {
+        builder.addCase(registerUser.pending,(state)=>{
+            state.loading = true
+            state.error = null
+        })
+        builder.addCase(registerUser.fulfilled,(state,{payload}) => {
+            state.loading = false
+            state.success = true
+        })
+        builder.addCase(registerUser.rejected, (state,{payload}) => {
+            state.loading = false
+            state.error = payload
+        })
+    }
 })
 
 export default authSlice.reducer
