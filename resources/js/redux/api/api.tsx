ab20189767875f0ef801ff651edfa44d6736d981
Northernexile <northernexile>
2023-01-25 14:09:27 +0000

    better token handling

--- resources/js/redux/api/api.tsx
+++ resources/js/redux/api/api.tsx
@@ -3,7 +3,11 @@ import axios from "axios";
 
 export default ()=>{
     // @ts-ignore
-    axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('userToken')}`;
+    const token = localStorage.getItem('userToken') ? localStorage.getItem('userToken') : null;
+
+    if(token !== null) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
     return axios.create({
         baseURL:'/api/'
     })
