63b8359734d87e3fd68fd05eb882965e540e2268
Northernexile <northernexile>
2023-02-01 17:18:52 +0000

    secure routes

--- routes/api/experience.php
+++ routes/api/experience.php
@@ -7,23 +7,23 @@ $sectionName = 'experience';
 
 Route::prefix('experience')->group(function ($request) use($sectionName){
 Route::get('/',[ExperienceController::class,'index'])
-    ->name($sectionName.'.index')->middleware(['auth:sanctum']);
+    ->name($sectionName.'.index')->middleware(['auth:sanctum','owner']);
 Route::get('/{id}',[ExperienceController::class,'show'])
     ->name($sectionName.'.show')
-    ->middleware(['auth:sanctum']);
+    ->middleware(['auth:sanctum','owner']);
 Route::patch('/{id}',[ExperienceController::class,'update'])
-    ->middleware(['auth:sanctum'])
+    ->middleware(['auth:sanctum','owner'])
     ->name($sectionName.'.save');
 Route::post('/',[ExperienceController::class,'create'])
-    ->middleware(['auth:sanctum'])
+    ->middleware(['auth:sanctum','owner'])
     ->name($sectionName.'.save');
 Route::delete('/',[ExperienceController::class,'deleteAll'])
-    ->middleware(['auth:sanctum'])
+    ->middleware(['auth:sanctum','owner'])
     ->name($sectionName.'.delete.all');
 Route::delete('/{id}',[ExperienceController::class,'delete'])
-    ->middleware(['auth:sanctum'])
+    ->middleware(['auth:sanctum','owner'])
     ->name($sectionName.'.delete');
 Route::get('/search/{term}',[ExperienceController::class,'search'])
-    ->middleware(['auth:sanctum'])
+    ->middleware(['auth:sanctum','owner'])
     ->name($sectionName.'.search');
 });
