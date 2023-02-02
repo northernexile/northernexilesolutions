e3fda46d047d54ba23cd08170401084ccdab9b45
Northernexile <northernexile>
2023-02-02 14:10:26 +0000

    protect some routes

--- routes/api/project.php
+++ routes/api/project.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 $sectionName = 'project';
 
-Route::prefix('project')->group(function ($request) use($sectionName){
-Route::get('/',[ProjectController::class,'index'])->name($sectionName.'.index');
-Route::get('/{id}',[ProjectController::class,'show'])->name($sectionName.'.show');
-Route::put('/{id}',[ProjectController::class,'update'])->name($sectionName.'.save');
-Route::post('/{id}',[ProjectController::class,'create'])->name($sectionName.'.save');
-Route::delete('/',[ProjectController::class,'deleteAll'])->name($sectionName.'.delete.all');
-Route::delete('/{id}',[ProjectController::class,'delete'])->name($sectionName.'.delete');
-Route::get('/search/{term}',[ProjectController::class,'search'])->name($sectionName.'.search');
+Route::prefix('project')->middleware(['auth:sanctum','owner'])->group(function ($request) use($sectionName){
+    Route::get('/',[ProjectController::class,'index'])->name($sectionName.'.index');
+    Route::get('/{id}',[ProjectController::class,'show'])->name($sectionName.'.show');
+    Route::put('/{id}',[ProjectController::class,'update'])->name($sectionName.'.save');
+    Route::post('/{id}',[ProjectController::class,'create'])->name($sectionName.'.save');
+    Route::delete('/',[ProjectController::class,'deleteAll'])->name($sectionName.'.delete.all');
+    Route::delete('/{id}',[ProjectController::class,'delete'])->name($sectionName.'.delete');
+    Route::get('/search/{term}',[ProjectController::class,'search'])->name($sectionName.'.search');
 });
