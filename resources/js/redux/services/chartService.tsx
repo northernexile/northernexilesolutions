161d0deb52e6f7d6e8fa9a188194284fbbf0f6da
Northernexile <northernexile>
2023-02-10 13:11:00 +0000

    add more services to charts

--- resources/js/redux/services/chartService.tsx
+++ resources/js/redux/services/chartService.tsx
@@ -1,8 +1,24 @@
 import Api from "../api/api";
 
 export default {
+    async getAll(){
+        return await Api().get('chart/all').then((response) => {
+            return response.data.data.chart
+        }).catch((error)=>{
+            return error.response.data
+        });
+    },
+
     async getFrameworks(){
         return await Api().get('chart/frameworks').then((response) => {
+            return response.data.data.chart
+        }).catch((error)=>{
+            return error.response.data
+        });
+    },
+
+    async getSectors(){
+        return await Api().get('chart/sectors').then((response) => {
             return response.data.data.chart
         }).catch((error)=>{
             return error.response.data
