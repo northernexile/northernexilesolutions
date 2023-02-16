fb2c8b17adc9fa55c0c2e857a83f5c2cbd1a620d
Northernexile <northernexile>
2023-02-16 19:34:28 +0000

    editing address

--- resources/js/components/Dashboard/Clients/Addresses/ClientAddressEdit.jsx
+++ resources/js/components/Dashboard/Clients/Addresses/ClientAddressEdit.jsx
@@ -1,35 +1,53 @@
 
 
-import React from "react";
+import React, {useEffect} from "react";
 import {Card, CardContent, CardHeader, Grid} from "@mui/material";
 import {Link, useParams} from "react-router-dom";
 import {cardStyle} from "../../../../snippets/cardStyle";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import getBrandIcon from "../../../../services/icons/icons";
 import ClientAddressEditData from "./ClientAddressEditData";
+import {viewClientAddress} from "../../../../redux/actions/clientAddressActions";
+import {useAppDispatch, useAppSelector} from "../../../../redux/hooks/hooks";
 export default function ClientAddressEdit() {
     let params = useParams()
+    const dispatch = useAppDispatch();
+    useEffect(()=>dispatch(viewClientAddress(params.id)),[])
+    const clientAddress = useAppSelector(state => state.clientAddress.clientAddress);
+
+    console.log(clientAddress)
+
+    const showCard = () => {
+        return clientAddress.client_id !== undefined
+    }
+
+    const card = () => {
+        return (
+            <Grid item xs={12}>
+                <Card elevation={2}
+                      style={cardStyle}
+                >
+                    <CardHeader
+                        className={`title-bar`}
+                        title={`View/Edit Client Address`}
+                        action={
+                            <>
+                                <Link to={`/dashboard/clients/`+clientAddress.client_id}>
+                                    <FontAwesomeIcon icon={getBrandIcon(`faArrowCircleLeft`)} />
+                                </Link>
+                            </>
+                        }
+                    />
+                    <CardContent>
+                        <ClientAddressEditData clientAddress={clientAddress} />
+                    </CardContent>
+                </Card>
+            </Grid>
+        )
+    }
 
     return (
-        <Grid item xs={12}>
-            <Card elevation={2}
-                  style={cardStyle}
-            >
-                <CardHeader
-                    className={`title-bar`}
-                    title={`View/Edit Client Address`}
-                    action={
-                        <>
-                            <Link to={`/dashboard/clients/`+params.id}>
-                                <FontAwesomeIcon icon={getBrandIcon(`faArrowCircleLeft`)} />
-                            </Link>
-                        </>
-                    }
-                />
-                <CardContent>
-                    <ClientAddressEditData />
-                </CardContent>
-            </Card>
-        </Grid>
+        <>{showCard() && card()}</>
     )
+
 }
--- resources/js/components/Dashboard/Clients/Addresses/ClientAddressEditData.jsx
+++ resources/js/components/Dashboard/Clients/Addresses/ClientAddressEditData.jsx
@@ -3,18 +3,65 @@ import React, {useEffect} from "react"
 import {useParams} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../../../redux/hooks/hooks";
 import {viewClientAddress} from "../../../../redux/actions/clientAddressActions";
+import {updateAddress} from "../../../../redux/actions/addressActions";
+import {FormProvider, useForm} from "react-hook-form";
+import FormRowInput from "../../../../controls/rows/FormRowInput";
+import Button from "@mui/material/Button";
+import {Save} from "@mui/icons-material";
+import Identity from "../../../../controls/Identity";
 
-const ClientAddressEditData = () => {
+const ClientAddressEditData = ({clientAddress}) => {
     const dispatch = useAppDispatch()
     const params = useParams();
-    const clientAddress = useAppSelector(state => state.clientAddress.clientAddress);
 
-    console.log(clientAddress);
+    const methods = useForm();
 
-    useEffect(()=>dispatch(viewClientAddress(params.id)),[])
+    const initialiseForm = () => {
+        let address = clientAddress.address
+        methods.setValue('id',address.id)
+        methods.setValue('thoroughfare',address.thoroughfare)
+        methods.setValue('address_line_1',address.address_line_1)
+        methods.setValue('address_line_2',address.address_line_2)
+        methods.setValue('address_line_3',address.address_line_3)
+        methods.setValue('town',address.town)
+        methods.setValue('county',address.county)
+        methods.setValue('postcode',address.postcode)
+    }
+
+    const save = (data) => {
+        dispatch(updateAddress(data))
+    }
+
+    const loaded = () => {
+        return clientAddress.id !==undefined
+    }
+
+    const form = () => {
+        initialiseForm()
+
+        return (
+            <FormProvider {...(methods)}>
+                <form onSubmit={methods.handleSubmit(save)}>
+                    <Identity name={`id`} control={methods.control} />
+                    <FormRowInput name={`thoroughfare`} control={methods.control} title={`Thoroughfare`} />
+                    <FormRowInput name={`address_line_1`} control={methods.control} title={`Address Line 1`} />
+                    <FormRowInput name={`address_line_2`} control={methods.control} title={`Address Line 2`} />
+                    <FormRowInput name={`address_line_3`} control={methods.control} title={`Address Line 3`} />
+                    <FormRowInput name={`town`} control={methods.control} title={`Town`} />
+                    <FormRowInput name={`county`} control={methods.control} title={`County`} />
+                    <FormRowInput name={`postcode`} control={methods.control} title={`Postcode`} />
+                    <div className={`form-row`}>
+                        <Button variant={`contained`} endIcon={<Save />} type={`submit`} title={`Save`} >
+                            Save Address
+                        </Button>
+                    </div>
+                </form>
+            </FormProvider>
+        )
+    }
 
     return (
-        <></>
+        <>{loaded() && form()}</>
     )
 }
 
